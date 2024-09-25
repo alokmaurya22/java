62b2b7a0fc33cacd0372694f75023c8ea08b3ae4
Alok Maurya <alokmauryaraj2003@gmail.com>
2024-09-25 13:06:53 +0530

    Java Basics and Datatypes

---
+++ 1_Basic_Datatypes/P2_Datatypes.java
@@ -0,0 +1,16 @@
+
+public class P2_Datatypes {
+
+    public static void main(String[] args) {
+        int num = 1223233232; //4bytes
+        short shortnum = 23; //2bytes
+        byte bytenum = 123; //1byte
+        long longnum = 123123123123123123L; //8bytes
+        float floatnum = 123.123f; //4bytes //single digit precision //7 digit representation
+        double doublenum = 123.123; //8bytes // double digit precision //14 digit representation
+        boolean booleannum = true; //1byte
+        char charecterData = 'a'; //2bytes
+
+    }
+
+}
